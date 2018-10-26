6d2686efc9a4c6f3324f1e014d48416025f18f2a
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 15:21:39 +0100

    Add test for addEntry method

--- test/addressBook.test.js
+++ test/addressBook.test.js
@@ -2,11 +2,20 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const AddressBook = require('../models/addressBook');
+const Person = require('../models/person');
 
 describe('AddressBook', () => {
   it('should initialize properly', () => {
     const book = new AddressBook();
 
     expect(book.entries).to.deep.equal([]);
+  });
+
+  it('should add a person to the book entries', () => {
+    const book = new AddressBook();
+    const person = new Person('luis', 'Amez', '03/08/1984');
+    book.addEntry(person);
+    
+    expect(book.entries).to.deep.equal([person]);
   });
 });
