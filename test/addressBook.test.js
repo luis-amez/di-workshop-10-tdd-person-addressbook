fec6a52a1ca3d3a97ce2faea142c8ddb7b3535cb
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 15:40:14 +0100

    Add test for findByFirstName method

--- test/addressBook.test.js
+++ test/addressBook.test.js
@@ -13,9 +13,23 @@ describe('AddressBook', () => {
 
   it('should add a person to the book entries', () => {
     const book = new AddressBook();
-    const person = new Person('luis', 'Amez', '03/08/1984');
+    const person = new Person('Luis', 'Amez', '03/08/1984');
     book.addEntry(person);
     
     expect(book.entries).to.deep.equal([person]);
+  });
+
+  it('should find persons by firstName', () => {
+    const book = new AddressBook();
+    const person1 = new Person('Luis', 'Amez', '03/08/1984');
+    const person2 = new Person('Fatima', 'Amez', '14/02/1988');
+    const person3 = new Person('Luis', 'Vitoria', '13/05/1994');
+    const person4 = new Person('Teresa', 'Romero', '08/12/1956');
+    book.addEntry(person1);
+    book.addEntry(person2);
+    book.addEntry(person3);
+    book.addEntry(person4);
+
+    expect(book.findByFirstName('Luis')).to.deep.equal([person1, person3]);
   });
 });
