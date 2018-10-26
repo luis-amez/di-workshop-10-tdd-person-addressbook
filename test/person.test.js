66e2b752c311fa3b5c498a3826a44f43e013dfd6
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 12:21:48 +0100

    Add test for addEmail method

--- test/person.test.js
+++ test/person.test.js
@@ -24,4 +24,12 @@ describe('Person', () => {
 
     expect(person.fullName()).to.equal('Luis Amez');
   });
+
+  it('should add an email account', () => {
+    const person = new Person('Luis', 'Amez', '03/08/1984');
+    person.addEmail('luis@mail.com');
+    person.addEmail('amez@mail.com');
+
+    expect(person.emails).to.deep.equal(['luis@mail.com', 'amez@mail.com']);
+  });
 });
