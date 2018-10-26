2e890869763b2b9e0d5c0a473b453439a84fe5c4
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 12:13:10 +0100

    Add functionality for capitalizing firstName

--- models/person.js
+++ models/person.js
@@ -1,8 +1,12 @@
 class Person {
   constructor(firstName, surname, dob) {
-    this.firstName = firstName;
+    this.firstName = this._capitalize(firstName);
     this.surname = surname;
     this.dob = dob;
+  }
+
+  _capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 }
 
