a3da0f38fd4c78b5f2275b417f9a67dfab6ccd12
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 15:40:49 +0100

    Add findByFirstName method

--- models/addressBook.js
+++ models/addressBook.js
@@ -6,6 +6,12 @@ class AddressBook {
   addEntry(person) {
     this.entries.push(person);
   }
+
+  findByFirstName(name) {
+    return this.entries.filter((entry) => {
+      return entry.firstName === name;
+    });
+  }
 }
 
 module.exports = AddressBook;
