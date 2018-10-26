89949f7650ed1dc77bd91f591b7bc2f827f6f93d
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 12:01:24 +0100

    Add test for Person constructor

--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,14 @@
-const chai = require('chai')
-const expect = chai.expect
+const chai = require('chai');
+const expect = chai.expect;
 
-// Your Person test goes here!
+const Person = require('../models/person');
+
+describe('Person', () => {
+  it('should initialize properly', () => {
+    const person = new Person('Luis', 'Amez', '03/08/1984');
+
+    expect(person.firstName).to.equal('Luis');
+    expect(person.surname).to.equal('Amez');
+    expect(person.dob).to.equal('03/08/1984');
+  });
+});
