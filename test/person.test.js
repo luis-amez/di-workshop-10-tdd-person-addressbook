f7b2b2a09e9e0a8e70f21adee984bdd43b2b140b
Luis Amez <luis.amez.vitoria@gmail.com>
2018-10-26 14:46:46 +0100

    Add test for returnFormattedDetails method

--- test/person.test.js
+++ test/person.test.js
@@ -40,4 +40,26 @@ describe('Person', () => {
 
     expect(person.phoneNumbers).to.deep.equal(['0123456789', '9876543210']);
   });
+
+  it('should return a resume with all details from user', () => {
+    const person = new Person('Luis', 'Amez', '03/08/1984');
+    person.addEmail('luis@mail.com');
+    person.addEmail('amez@mail.com');
+    person.addPhoneNumber('0123456789');
+    person.addPhoneNumber('9876543210');
+
+    expect(person.returnFormattedDetails()).to.equal(
+      "Luis Amez\n" +
+      "----------\n" +
+      "DOB: 03/08/1984\n" +
+      "\n" +
+      "Email Addresses:\n" +
+      "- luis@mail.com\n" +
+      "- amez@mail.com\n" +
+      "\n" +
+      "Phone Numbers:\n" +
+      "- 0123456789\n" +
+      "- 9876543210\n"
+    )
+  });
 });
